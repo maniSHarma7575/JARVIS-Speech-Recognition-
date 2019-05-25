8e90ce709bff3663a4ef4c8e0a1dc5b84ca6f37c
Manish sharma <hacker.sharma7575@gmail.com>
2019-05-25 16:15:45 +0530

    first commit

---
+++ speech1.py/venv/spee.py
@@ -0,0 +1,114 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+from playsound import playsound
+import smtplib
+
+engine = pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('rate',150)
+engine.setProperty('voice',voices[16].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am Your Assistant Lolipop. Please tell me how may I help you")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source,duration=0.5)
+        r.pause_threshold=1
+        audio = r.listen(source)
+
+    try:
+        speak("Please wait connecting you to internet")
+        query = r.recognize_google(audio,language='en-in')
+        speak(query)
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('ms90051@gmail.com', '9411561648')
+    server.sendmail('ms90051@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    count=0
+    while True:
+        if count>=1:
+            speak(("Sir next Command!"))
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            google='youtube.com'
+            webbrowser.open_new_tab('http://www.google.com/search?btnG=1&q=%s' % google)
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+
+        elif 'play music' in query:
+
+            music_dir = '/home/manish/Documents/Manish Window/Music'
+            songs = os.listdir(music_dir)
+            #mixer.init()
+            #mixer.music.load(music_dir+"/"+songs[0])
+            #mixer.music.play()
+            playsound(music_dir+"/"+songs[0])
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+
+        elif 'email to Manish' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "sharma.manish7575@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry Sir, I am not able to send this email")
+        else:
+            speak("Good Bye Sir")
+            exit()
+        count=count+1
